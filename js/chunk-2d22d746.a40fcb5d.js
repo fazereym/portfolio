692e6a03bf4575de30dc8e73778fe7c68fdafc51
reymuhamad <53169904+fazereym@users.noreply.github.com>
2020-08-10 10:59:33 +0700

    Test Deploy

---
+++ js/chunk-2d22d746.a40fcb5d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(t,e,n){"use strict";n.r(e);var u=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},r=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{attrs:{id:"About"}},[n("h1",[t._v("About")])])}],c={},l=c,s=n("2877"),a=Object(s["a"])(l,u,r,!1,null,null,null);e["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.a40fcb5d.js.map
